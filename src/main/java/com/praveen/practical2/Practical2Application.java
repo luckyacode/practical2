79d2dfab1c69b73bf35e04c8417dbb070edd7bb0
praveen tripathi <lifesstudy@gmail.com>
2024-11-23 17:55:22 +0530

    Initial commit

---
+++ src/main/java/com/praveen/practical2/Practical2Application.java
@@ -0,0 +1,13 @@
+package com.praveen.practical2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practical2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practical2Application.class, args);
+	}
+
+}
